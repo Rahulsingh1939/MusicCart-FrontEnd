64ddf57fd2593680522b5781c2f0887974cd729d
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-12-26 22:58:15 +0530

    Work Going On UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
 import './App.css'
+
+import Layout from './components/Layout/Layout'
+import Home from './pages/Home/Home'
 
 function App() {
 
   return (
     <>
+    <Layout>
+      <div className="container"><Home /></div>
+    
+    </Layout>
+   
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
 import App from './App.jsx'
+import Signin from './pages/Signin/Signin.jsx';
+import Register from './pages/Register/Register.jsx';
+import Home from './pages/Home/Home.jsx';
+import PageNotFound from './pages/PageNotFound/PageNotFound.jsx';
+
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/*",
+    element: <PageNotFound />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
