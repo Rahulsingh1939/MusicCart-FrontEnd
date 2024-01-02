80d8bda500057059a06a4a08057a62fbb809e36f
Rahulsingh1939 <rahulsingh19390@gmail.com>
2024-01-02 11:29:26 +0530

    Working on Cart Functionality

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,15 +1,88 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
-import StyledComponent from '../../components/Banner/Head'
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+import StyledComponent from "../../components/Banner/Head";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const CartPage = () => {
+    const navigate=useNavigate();
   return (
     <>
-    <Layout>
-        <StyledComponent />
-    </Layout>
+      <Layout>
+        <StyledComponent location="Cart"/>
+        <Container
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <span>Back to products</span>
+        </Container>
+        <CartLogo>
+          <Image
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/4a9c56f36d69faf7762fb247a7245d88e1349cf9de003a2f30d742452d5c1741?apiKey=c41df0b048fb4bad873f2d9b07bfce38&"
+          />
+          <Title>My Cart</Title>
+        </CartLogo>
+        
+      </Layout>
     </>
-  )
-}
+  );
+};
+const Container = styled.button`
+  color: #fff;
+  white-space: nowrap;
+  border-radius: 12px;
+  background-color: #2e0052;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 8vw;
+  height: 5vh;
+  margin-left: 10vw;
+  font: 500 100% Roboto, sans-serif;
+  cursor: pointer;
 
-export default CartPage
+  @media (max-width: 991px) {
+    white-space: initial;
+    margin: 40px 0 0 10px;
+    padding: 0 20px;
+  }
+`;
+const CartLogo = styled.div`
+  display: flex;
+  margin: 3%;
+
+  gap: 20px;
+  width: 100vw;
+  height: 4vh;
+justify-content:center;
+align-items: center;
+
+  @media (max-width: 991px) {
+    margin-top: 40px;
+  }
+`;
+
+const Image = styled.img`
+  aspect-ratio: 1.03;
+  object-fit: contain;
+  object-position: center;
+  width: 3vw;
+  fill: #000;
+  margin-left:42vw;
+  overflow: hidden;
+  max-width: 100%;
+`;
+
+const Title = styled.span`
+  color: #000;
+  margin-top: 21px;
+  flex-grow: 1;
+  white-space: nowrap;
+  font: 700 40px Roboto, sans-serif;
+  @media (max-width: 991px) {
+    white-space: initial;
+  }
+`;
+export default CartPage;
